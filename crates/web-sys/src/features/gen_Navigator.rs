3f4ed802bc94fd171b1c4d2a548d8159b4fd937e
Nathan Dobson <nadobson@mac.com>
2026-05-02 11:33:20 -0700

    Make window.navigator.usb fallible.

--- crates/web-sys/src/features/gen_Navigator.rs
+++ crates/web-sys/src/features/gen_Navigator.rs
@@ -192,7 +192,7 @@ extern "C" {
     #[doc = ""]
     #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
     #[doc = "[described in the `wasm-bindgen` guide](https://wasm-bindgen.github.io/wasm-bindgen/web-sys/unstable-apis.html)*"]
-    pub fn usb(this: &Navigator) -> Usb;
+    pub fn usb(this: &Navigator) -> Option<Usb>;
     #[cfg(web_sys_unstable_apis)]
     #[cfg(feature = "XrSystem")]
     # [wasm_bindgen (structural , method , getter , js_class = "Navigator" , js_name = xr)]
